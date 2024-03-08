d4fcbc76d9daa1292900da5ccf21f4d8fb2f2509
PrzeChoj <premysl.choj@gmail.com>
2024-03-08 17:09:49 +0100

    PEOs bugfix and documentation

--- R/perfect_elimination_orderings.R
+++ R/perfect_elimination_orderings.R
@@ -1,3 +1,20 @@
+#' Find out if the vertev v is simplicial
+#'
+#' @inheritParams perfect_elimination_orderings
+#' @param v vertex of interest
+#'
+#' @export
+#'
+#' @examples
+#' M <- matrix(c(
+#'   0, 0, 0, 0,
+#'   0, 0, 1, 0,
+#'   0, 1, 0, 1,
+#'   0, 0, 1, 0
+#' ), nrow = 4) # 1 2-3-4
+#' sapply(1:4, function(v) {
+#'   is_simplicial(v, M)
+#' }) # TRUE, TRUE, FALSE, TRUE
 is_simplicial <- function(v, adj) { # adj - macierz wymiaru dim, v to element 1:dim
   ne <- which(adj[v, ] == 1)
   for (x in ne) {
@@ -10,6 +27,20 @@ is_simplicial <- function(v, adj) { # adj - macierz wymiaru dim, v to element 1:
   return(TRUE)
 }
 
+#' Find all the Perfect Elimination Orderings (PEOs)
+#'
+#' @param adj Adjacency matrix of a graph of interest
+#'
+#' @export
+#'
+#' @examples
+#' M <- matrix(c(
+#'   0, 0, 0, 0,
+#'   0, 0, 1, 0,
+#'   0, 1, 0, 1,
+#'   0, 0, 1, 0
+#' ), nrow = 4) # 1 2-3-4
+#' perfect_elimination_orderings(M) # list of 16 PEOs
 perfect_elimination_orderings <- function(adj) { # adj - macierz wymiaru co najmniej 2x2
   n <- nrow(adj)
   if (n == 2) {
@@ -22,7 +53,7 @@ perfect_elimination_orderings <- function(adj) { # adj - macierz wymiaru co najm
       suborders <- perfect_elimination_orderings(adj[-c(v), -c(v)])
       # Znajdujemy doskonale porzadki w grafie bez wierzcholka v
       # Usuniecie wierzcholka v zaburza nam numeracje
-      # Do kazdego z otrzymanych porzadkow chcemy dopisac wierzcholek v na koncu
+      # Do kazdego z otrzymanych porzadkow chcemy dopisac wierzcholek v na poczatku
       # Musimy uwzglednic zmiane numeracji
       for (j in 1:length(suborders)) {
         for (k in 1:length(suborders[[j]]))
@@ -31,7 +62,7 @@ perfect_elimination_orderings <- function(adj) { # adj - macierz wymiaru co najm
             suborders[[j]][k] <- suborders[[j]][k] + 1
           }
         }
-        order <- c(suborders[[j]], v) # na koniec kladziemy wierzcholek simplicjalny
+        order <- c(v, suborders[[j]]) # na poczatek kladziemy wierzcholek simplicjalny
         orders[[length(orders) + 1]] <- order
       }
     }
